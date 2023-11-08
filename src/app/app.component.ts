429967cacf2f50be74c8b6535a05b577ae77ee2f
Ehamelo Efiareta <efiareta.ehamelo@meritis.fr>
2023-11-08 21:09:48 +0100

    minor refactoring, removve useless control variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent implements OnInit {
 
     dialogRef.componentInstance.submitClicked.subscribe((aModele: Modele) => {
       this.addNewModel(aModele);
-      dialogRef?.close();
+      dialogRef.close();
     });
   }
 }
